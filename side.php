ec8f284c755cd408e18b9677c69f8dd426d47e5b
jeanphilippelevy <jeanphilippe.levy@gmail.com>
2016-02-26 16:22:18 +0100

    Various css fixes

--- side.php
+++ side.php
@@ -27,7 +27,7 @@ $menus = $m::createPHPDictionnary();
 ?>
 
 <!-- Nav menu -->
-<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+<nav class="navbar navbar-default navbar-static-top" style="margin-bottom: 0">
 	<div class="navbar-header">
 		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
 			<span class="sr-only">Toggle navigation</span>
