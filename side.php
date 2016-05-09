0f1f8285b3266ea28686a65bd74ff2e5f0bf98e0
QuentinHoarau <quentin.hoarau@axians.com>
2016-05-09 17:19:48 +0200

    display username in menu

--- side.php
+++ side.php
@@ -50,10 +50,10 @@ $menus = $m::createPHPDictionnary();
 	<ul class="nav navbar-top-links navbar-right">
 		<li class="dropdown">
 			<a class="dropdown-toggle" data-toggle="dropdown" href="#">
-				<i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
+				<i class="fa fa-user fa-fw"></i> <?php echo ucfirst($_COOKIE['user_name']); ?> <i class="fa fa-caret-down"></i>
 			</a>
 			<ul class="dropdown-menu dropdown-user">
-				<li><a href="/password.php"><i class="fa fa-user fa-fw"></i> <?php echo getLabel("menu.user.profile"); ?></a>
+				<li><a href="/module/module_password/index.php"><i class="fa fa-user fa-fw"></i> <?php echo getLabel("menu.user.profile"); ?></a>
 				</li>
 				<li class="divider"></li>
 				<li><a href="/logout.php"><i class="fa fa-sign-out fa-fw"></i> <?php echo getLabel("menu.user.disconnect"); ?></a>
